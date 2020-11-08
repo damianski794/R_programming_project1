b0ca42d8bfebaf0a257b2daf7c1ba20a2121f2ae
Damian Wysokinski <damianwysokinski@o2.pl>
2020-11-08 22:19:07 +0100

    final version od spectral file

--- spectral.R
+++ spectral.R
@@ -1,43 +1,36 @@
-spectral_clustering <- function(X, k, M){
-  # X - macierz n x d
-  # k - liczba roznych grup danych
-  # M - liczba najblizszych sasiadow
+spectral_clustering <- function(X, k, M, print_graph = FALSE){
+  # X - array n x d in which we want to find clusters
+  # k - number of distinct groups (clusters)
+  # M - number of nearest neighbours
   
-  # returns labels
+  # returns labels of found clusters
   
   S <- Mnn(X, M)
-  G <- Mnn_graph(S, X)
+  G <- Mnn_graph(S, X, print_graph)
   E <- Laplacian_eigen(G,k)
-  print(kmeans(x = E, centers = k))
+
   labels <- kmeans(x = E, centers = k)$cluster
-  print('labels')
-  print(labels)
+
   return(labels)
 }
 
 
 Mnn <- function(X, M = 3){
+  # X - array n x d
+  # M - number of nearest neighbours
+  
+  # returns S array n x M with indicies of nearest neighbours of each point 
   
   n <- nrow(X)
   d <- ncol(X)
   
   S = matrix(0.0, nrow = n, ncol = 1)
   
-  #X
   
   distances <- as.matrix(dist(X))
-  #distances
   
   S <- matrix(0.0,nrow = n, ncol = M)
-  S
-  #distances[2,]
   
-  #order(distances[2,], decreasing = FALSE)
-  #order(distances[2,], decreasing = FALSE)[2:(M+1)]
-  
-  #S[2,] <- order(distances[2,], decreasing = FALSE)[2:(M+1)]
-  #S
-  #n
   for (i in 1:n){
     S[i,] <- order(distances[i,], decreasing = FALSE)[2:(M+1)]
   }
@@ -45,9 +38,17 @@ Mnn <- function(X, M = 3){
   return(S)
 }
 
-Mnn_graph <- function(S, X){
+Mnn_graph <- function(S, X, print_graph = FALSE){
+  # creates adjecency matrix from connected graph
+  # prints graph if print_graph = TRUE
+  
+  # S - adjecency list (array)
+  # X - data with points positions
+  
+  #returns connected adjecency matrix
+  
   S_n_rows = nrow(S)
-  G <- matrix(0.0, nrow = S_n_rows, ncol = S_n_rows) #symetric matrix 20x20
+  G <- matrix(0.0, nrow = S_n_rows, ncol = S_n_rows)
   
   for (i in 1:S_n_rows){
     for (j in 1:ncol(S)) {
@@ -55,22 +56,17 @@ Mnn_graph <- function(S, X){
       G[S[i,j],i] <- 1;
     }
   }
-  print_first_graph_lines(G,X)
-  print('Przed uswpolnianiem')
+  if(print_graph){
+    print_first_graph_lines(G,X)
+  }
+  
   graph1 <- graph_from_adjacency_matrix(adjmatrix = G, mode='undirected')
-  print('LICZBA KOMPONENTOW')
-  print(count_components(graph1))
-  
-  #keep_going = TRUE
-  
-  print_first_graph_lines(G,X)
+
   
   n_connected_comp <- count_components(graph1)
   while(n_connected_comp != 1){
     rand_row <- sample(1:nrow(G),1)
     rand_col <- sample(1:nrow(G),1)
-    print(rand_row)
-    print(rand_col)
     if(G[rand_row,rand_col]==1){
       next
     }
@@ -79,8 +75,10 @@ Mnn_graph <- function(S, X){
       G[rand_col,rand_row]=1
       graph1 <- graph_from_adjacency_matrix(adjmatrix = G, mode='undirected')
       n_connected_comp <- count_components(graph1)
-      lines(c(blobs$samples[rand_row,1],blobs$samples[rand_col,1]),c(blobs$samples[rand_row,2],blobs$samples[rand_col,2]), type = 'l', col='red')
       
+      if(print_graph){
+        lines(c(blobs$samples[rand_row,1],blobs$samples[rand_col,1]),c(blobs$samples[rand_row,2],blobs$samples[rand_col,2]), type = 'l', col='red')
+      }
     }
   }
   
@@ -89,45 +87,28 @@ Mnn_graph <- function(S, X){
 }
 
 Laplacian_eigen <- function(G,k){
+  # G - adjencency matrix n x n
+  # k - number of distinct groups/clusters
+  
+  # returns E - laplacian array n x k
   D <- apply(G,1, sum)
-  print('D')
-  print(D)
   D <- diag(D)
-  print('d_diagonal')
-  print(D)
-  
   L <- D - G
-  
-  print('L')
-  print(L)
-  
-  # E nxk
-  #E <- matrix(0.0, nrow = nrow(G), ncol = k)
-  #print('E')
-  #print(E)
-  
-  print('eigen_values')
+
   ev <- eigen(L)$values
-  print(ev)
-  print('order of values')
-  print(order(ev, decreasing = FALSE))
   smallest_lambas_indices <- order(ev, decreasing = FALSE)[2:(k+1)]
-  print('smallest lambdas_idices')
-  print(smallest_lambas_indices)
-  
-  print('eigen(L)$vectors')
-  print(eigen(L)$vectors)
-  
-  print('eigen(L)$vectors[,smallest_lambas_indices]')
-  print(eigen(L)$vectors[,smallest_lambas_indices])
   
   E <- eigen(L)$vectors[,smallest_lambas_indices]
-  
   
   return(E)
 }
 
 print_first_graph_lines <- function(G, X){
+  # prints graph from adjececy matrix and points data
+  
+  # G - adjencency matrix n x n
+  # X - data with 2D points
+  
   for (i in 1:nrow(G)){
     for (j in 1:ncol(G)) {
       if (G[i,j] == 1){
@@ -136,11 +117,4 @@ print_first_graph_lines <- function(G, X){
     }
   }
 }
-print_added_line <- function(G,X){
-  #to be implemented
-}
 
-print_eigenvalues <- function(G){
-  evalues <- eigen(G)$values
-  print(evalues)
-}
